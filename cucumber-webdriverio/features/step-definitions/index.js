3ad6b5f5691516e1258bac2985d1b4e6c466a830
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2018-02-27 21:26:52 -0500

    1.4 Cucumber

---
+++ cucumber-webdriverio/features/step-definitions/index.js
@@ -0,0 +1,39 @@
+var {defineSupportCode} = require('cucumber');
+var {expect} = require('chai');
+
+defineSupportCode(({Given, When, Then}) => {
+  Given('I go to losestudiantes home screen', () => {
+    browser.url('/');
+    if(browser.isVisible('button=Cerrar')) {
+      browser.click('button=Cerrar');
+    }
+  });
+
+  When('I open the login screen', () => {
+    browser.waitForVisible('button=Ingresar', 5000);
+    browser.click('button=Ingresar');
+  });
+
+  When(/^I fill with (.*) and (.*)$/ , (email, password) => {
+     var cajaLogIn = browser.element('.cajaLogIn');
+
+    var mailInput = cajaLogIn.element('input[name="correo"]');
+    mailInput.click();
+    mailInput.keys(email);
+
+    var passwordInput = cajaLogIn.element('input[name="password"]');
+    passwordInput.click();
+    passwordInput.keys(password)
+  });
+
+  When('I try to login', () => {
+    var cajaLogIn = browser.element('.cajaLogIn');
+    cajaLogIn.element('button=Ingresar').click()
+  });
+
+  Then('I expect to see {string}', error => {
+    browser.waitForVisible('.aviso.alert.alert-danger', 5000);
+    var alertText = browser.element('.aviso.alert.alert-danger').getText();
+    expect(alertText).to.include(error);
+  });
+  });
